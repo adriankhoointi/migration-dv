b33fc7e87ba94e577f84fa0e2fbb365975d6c7ab
adriankhoointi <114915384+adriankhoointi@users.noreply.github.com>
2023-10-30 10:49:48 +0800

    Updated source template

--- assets/d3/vis/vis3-sd.js
+++ assets/d3/vis/vis3-sd.js
@@ -21,10 +21,8 @@ async function init() {
 //Load datasets
 async function loadDatasets() {
     //Load world json
-    worldjson = await d3.json("../assets/d3/data/countries.geojson");
+    worldjson = await d3.json("../assets/d3/data/countries-min.json");
 }
-
-[-2.218, 115.6628]
 
 function enter (svg) {
     //Set Up Globe Projection
@@ -54,7 +52,7 @@ function enter (svg) {
                     .attr("stroke-width", 0.2);
 
     //Define drag behaviour - allows user to rotate globe
-    svg.call(d3.drag().on('drag', (event) => {
+    svg.call(d3.drag().on('drag', _.debounce((event) => {
         //Get current rotation
         const rotate = projection.rotate();
         //Get dynamic sensitivity
@@ -66,20 +64,30 @@ function enter (svg) {
             rotate[1] - event.dy * k
         ]);
 
+        //Debug rotation
+        console.log(projection.rotate());
+
         //Update path
         path = d3.geoPath().projection(projection);
         svg.selectAll("path").attr("d", path);
-        }))
+
+        //Delay of debounce
+        })
+        ))
 
         //Define zoom behaviour
-        .call(d3.zoom().on('zoom', (event) => {
+        .call(d3.zoom().on('zoom', _.debounce((event) => {
             if(event.transform.k > 0.5) {
                 //Update projection
                 projection.scale(initialScale * event.transform.k);
+
+                //Debug scale
+                console.log(projection.scale());
+
                 //Update path
                 path = d3.geoPath().projection(projection);
                 svg.selectAll("path").attr("d", path);
-                
+
                 //Update globe radius
                 globe.attr("r", projection.scale());
             }
@@ -87,7 +95,10 @@ function enter (svg) {
                 //Reset zoom
                 event.transform.k = 0.5;
             }
-        }));
+        }
+        //Delay of debounce
+        )
+        ));
 
     //Stores the map
     let map = svg.append("g"); 
@@ -99,7 +110,7 @@ function enter (svg) {
         .enter()
         .append("path")
         //Replace space with underscores for class name
-        .attr("class", d => "country_" + d.properties.ADMIN.replace(" ", "_"))
+        .attr("class", d => "country_" + d.properties.name_en.replace(" ", "_"))
         //Appends the path to the map
         .attr("d", path)
 
