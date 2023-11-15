4900847cd74e90fbcb945611432cf3f1818a013b
MSNumberNine <103513174@student.swin.edu.au>
2023-11-15 19:38:40 +0800

    Symmetric horizontal axis

--- assets/d3/vis/vis4-pe.js
+++ assets/d3/vis/vis4-pe.js
@@ -149,7 +149,9 @@ function StackedBarChart(
   if (xDomain === undefined) xDomain = d3.extent(series.flat(2));
 
   // Construct scales, axes, and formats.
-  const xScale = xType(xDomain, xRange);
+  let maxAbsoluteValue = d3.max(data, d => Math.abs(d.emigration_count)); // Find the maximum absolute value in the data
+  const xScale = xType([-maxAbsoluteValue, maxAbsoluteValue], xRange); // Produce symmetric horizontal axis
+  // const xScale = xType(xDomain, xRange); // Produce asymmetric horizontal axis to construct scales, axes, and formats
   const yScale = d3.scaleBand(yDomain, yRange).paddingInner(yPadding);
   const color = d3.scaleOrdinal(zDomain, colors);
   const xAxis = d3.axisTop(xScale).ticks(w / 80, xFormat);
